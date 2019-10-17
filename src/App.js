454560bd41ebbadc706f29f7bfcc9fddd7e5b200
romluc <romulofilhofilho@gmail.com>
2019-10-17 16:36:46 -0300

    Create Header component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import './App.css';
 
@@ -43,6 +44,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className='App'>
+				<Header />
 				<Todos
 					todos={this.state.todos}
 					markComplete={this.markComplete}
